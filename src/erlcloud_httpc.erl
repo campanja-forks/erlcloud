5cebb90bb55f0acb147caf487abd23123d5a5a96
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2014-07-23 13:35:45 +0200

    Use lhttpc:header_value instead of proplists
    HTTP headers are supposed to be case-insensitive, but this is not
    respected if we use proplists:get_value to fetch headers.
    
    If we use the patched version of lhttpc in erlcloud 0.4.1 this doesn't
    matter since (apparently?) all headers are converted to lowercase.
    However, with other versions of lhttpc, we risk not finding header
    values due to case mismatches.

--- src/erlcloud_httpc.erl
+++ src/erlcloud_httpc.erl
@@ -9,7 +9,15 @@
 
 -module(erlcloud_httpc).
 
--export([request/6]).
+-export([request/6,
+         header_value/2,
+         header_value/3]).
 
 request(URL, Method, Hdrs, Body, Timeout, _Config) ->
     lhttpc:request(URL, Method, Hdrs, Body, Timeout, []).
+
+header_value(Key, Hdrs) ->
+    lhttpc_lib:header_value(Key, Hdrs).
+
+header_value(Key, Hdrs, Default) ->
+    lhttpc_lib:header_value(Key, Hdrs, Default).
